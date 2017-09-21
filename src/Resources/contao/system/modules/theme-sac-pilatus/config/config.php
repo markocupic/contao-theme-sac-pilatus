0b0a3ddc437f6c6f76c662ed9de2456729403b0c
Marko Cupic <m.cupic@gmx.ch>
2017-09-21 22:03:06 +0200

    Added touch punch

--- src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
+++ src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
@@ -24,8 +24,11 @@ if (TL_MODE == 'FE')
     $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-sac-pilatus/js/theme.js';
 
     // Embed JQuery UI
-	$GLOBALS['TL_HEAD'][] = '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>';
+    $GLOBALS['TL_HEAD'][] = '<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>';
 
-	// Embed jQuery touch swipe
-	$GLOBALS['TL_HEAD'][] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.min.js"></script>';
+    // Embed jQuery touch swipe
+    $GLOBALS['TL_HEAD'][] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.min.js"></script>';
+    // Enable dragging on touch screens
+    $GLOBALS['TL_HEAD'][] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>';
+
 }
