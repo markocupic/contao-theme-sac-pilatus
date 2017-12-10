39bef3e56dfb223cf6bd274181621fed8cf4f496
Marko Cupic <m.cupic@gmx.ch>
2017-12-10 21:46:26 +0100

    Added Fontawesome 5 compat

--- src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
+++ src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
@@ -1,58 +1,61 @@
 <?php
 if (TL_MODE == 'FE')
 {
-	// Head tags
-	$GLOBALS['TL_HEAD'][] = '<meta name="author" content="Marko Cupic, Oberkirch">';
-	$GLOBALS['TL_HEAD'][] = '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">';
+    // Head tags
+    $GLOBALS['TL_HEAD'][] = '<meta name="author" content="Marko Cupic, Oberkirch">';
+    $GLOBALS['TL_HEAD'][] = '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">';
 
 
-	// Bootstrap.js  Load in layout
-	//$GLOBALS['TL_JAVASCRIPT'][] = "https://code.jquery.com/jquery-3.2.1.slim.min.js";
-	//$GLOBALS['TL_JAVASCRIPT'][] = "https://code.jquery.com/jquery-1.12.4.min.js";
+    // Bootstrap.js  Load in layout
+    //$GLOBALS['TL_JAVASCRIPT'][] = "https://code.jquery.com/jquery-3.2.1.slim.min.js";
+    //$GLOBALS['TL_JAVASCRIPT'][] = "https://code.jquery.com/jquery-1.12.4.min.js";
 
-	// Popper.js must be loaded before bootstrap.js
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js";
+    // Popper.js must be loaded before bootstrap.js
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js";
 
-	// Bootstrap.js
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js";
+    // Bootstrap.js
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js";
 
-	// Icheck
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js";
-	$GLOBALS['TL_CSS'][] = "https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/green.css";
+    // Icheck
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js";
+    $GLOBALS['TL_CSS'][] = "https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/green.css";
 
-	// Bootstrap datepicker
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js";
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.de.min.js";
-	$GLOBALS['TL_CSS'][] = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.standalone.css";
+    // Bootstrap datepicker
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/js/bootstrap-datepicker.min.js";
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/locales/bootstrap-datepicker.de.min.js";
+    $GLOBALS['TL_CSS'][] = "https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.standalone.css";
 
-	// Load FontAwesome
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://use.fontawesome.com/926b4fc2c0.js";
+    // Load FontAwesome
+    //$GLOBALS['TL_JAVASCRIPT'][] = "https://use.fontawesome.com/926b4fc2c0.js";
+    $GLOBALS['TL_HEAD'][] = '<link href="/files/fontawesome-pro-5.0.1/web-fonts-with-css/css/fontawesome-all.css" rel="stylesheet">';
+    $GLOBALS['TL_HEAD'][] = '<script defer src="files/fontawesome-pro-5.0.1/svg-with-js/js/fontawesome-all.js"></script>';
 
-	// Animate
-	$GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
-	$GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css';
 
-	// Google Webfonts
-	//$GLOBALS['TL_CSS'][] = 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i';
+    // Animate
+    $GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js';
+    $GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css';
+
+    // Google Webfonts
+    //$GLOBALS['TL_CSS'][] = 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i';
     $GLOBALS['TL_CSS'][] = 'https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i';
 
-	// Slick: The last slider yo ever need
-	//$GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.js';
-	//$GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css';
-	//$GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.css';
+    // Slick: The last slider yo ever need
+    //$GLOBALS['TL_JAVASCRIPT'][] = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.js';
+    //$GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick-theme.css';
+    //$GLOBALS['TL_CSS'][] = 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.7.1/slick.min.css';
 
 
-	// Embed Drag Sortable Plugin from https://rubaxa.github.io/Sortable/
-	// JQuery UI doesn't go with popper.js
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js";
+    // Embed Drag Sortable Plugin from https://rubaxa.github.io/Sortable/
+    // JQuery UI doesn't go with popper.js
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js";
 
-	// Enable jqueryTouchSwipe
-	$GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.min.js";
+    // Enable jqueryTouchSwipe
+    $GLOBALS['TL_JAVASCRIPT'][] = "https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.18/jquery.touchSwipe.min.js";
 
-	// Load headroom.js https://github.com/WickyNilliams/headroom.js/
-  $GLOBALS['TL_HEAD'][] = '<script src="files/theme-sac-pilatus/js/headroom.js"></script>';
+    // Load headroom.js https://github.com/WickyNilliams/headroom.js/
+    $GLOBALS['TL_HEAD'][] = '<script src="files/theme-sac-pilatus/js/headroom.js"></script>';
 
-	// Load theme javascript file
-	$GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-sac-pilatus/js/theme.js';
+    // Load theme javascript file
+    $GLOBALS['TL_JAVASCRIPT'][] = 'files/theme-sac-pilatus/js/theme.js';
 
 }
