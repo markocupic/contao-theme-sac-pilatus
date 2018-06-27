31a12fd818300a789d72c4ab999e11c4ff93f862
Marko Cupic <m.cupic@gmx.ch>
2018-06-27 15:19:54 +0200

    switched to bootstrap 4.1.1

--- src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
+++ src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
@@ -26,8 +26,8 @@ if (TL_MODE == 'FE')
 
     // Load FontAwesome
     //$GLOBALS['TL_JAVASCRIPT'][] = "https://use.fontawesome.com/926b4fc2c0.js";
-    $GLOBALS['TL_CSS'][] = 'files/fontawesome-pro-5.0.6/web-fonts-with-css/css/fontawesome-all.min.css|static';
-    $GLOBALS['TL_HEAD'][] = '<script defer src="files/fontawesome-pro-5.0.6/svg-with-js/js/fontawesome-all.min.js"></script>';
+    $GLOBALS['TL_CSS'][] = 'files/fontawesome-pro-5.0.13/web-fonts-with-css/css/fontawesome-all.min.css|static';
+    $GLOBALS['TL_HEAD'][] = '<script defer src="files/fontawesome-pro-5.0.13/svg-with-js/js/fontawesome-all.min.js"></script>';
 
 
     $GLOBALS['TL_HEAD'][] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.min.js"></script>';
