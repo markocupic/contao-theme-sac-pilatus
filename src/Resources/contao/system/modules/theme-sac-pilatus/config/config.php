d66a0cb959bf855a0419f8237aea42f19db502ff
Marko Cupic <m.cupic@gmx.ch>
2019-06-09 10:12:27 +0200

    dev

--- src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
+++ src/Resources/contao/system/modules/theme-sac-pilatus/config/config.php
@@ -7,10 +7,10 @@ if (TL_MODE == 'FE')
 
     // JQuery is loaded in the layout
     // Popper.js must be loaded before bootstrap.js
-    $GLOBALS['TL_HEAD'][] = '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>';
+    $GLOBALS['TL_HEAD'][] = '<script src="assets/contao-component-popper-js/popper.min.js"></script>';
 
     // Bootstrap.js
-    $GLOBALS['TL_HEAD'][] = '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>';
+    $GLOBALS['TL_HEAD'][] = '<script src="assets/contao-component-bootstrap/bootstrap/dist/js/bootstrap.min.js"></script>';
 
     // Load FontAwesome
     $GLOBALS['TL_CSS'][] = '/files/fontawesome-pro-5.5.0-web/css/all.css|static';
