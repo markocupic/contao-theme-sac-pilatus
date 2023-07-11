6d1a4abf059770cd4a4487e5ccee6e4bc8279fba
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-07-11 07:33:23 +0200

    Fix ci issue

--- tools/ecs/batch/ecs_fixer.bat
+++ tools/ecs/batch/ecs_fixer.bat
@@ -7,6 +7,6 @@ cd..
 cd..
 cd..
 php vendor\bin\ecs check vendor/markocupic/contao-theme-sac-pilatus/src --fix --config vendor/markocupic/contao-theme-sac-pilatus/tools/ecs/config.php
-php vendor\bin\ecs check vendor/markocupic/contao-theme-sac-pilatus/symlink_stuff/templates --fix --config vendor/markocupic/contao-theme-sac-pilatus/tools/ecs/config.php
+php vendor\bin\ecs check vendor/markocupic/contao-theme-sac-pilatus/contao/templates --fix --config vendor/markocupic/contao-theme-sac-pilatus/tools/ecs/config.php
 php vendor\bin\ecs check vendor/markocupic/contao-theme-sac-pilatus/config --fix --config vendor/markocupic/contao-theme-sac-pilatus/tools/ecs/config.php
 php vendor\bin\ecs check vendor/markocupic/contao-theme-sac-pilatus/tests --fix --config vendor/markocupic/contao-theme-sac-pilatus/tools/ecs/config.php
