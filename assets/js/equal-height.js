cd684e3cf7fa7720decde41aca651e514586d318
Marko Cupic <m.cupic@gmx.ch>
2026-03-22 12:41:15 +0100

    Refactor the equal-height script for cards

--- assets/js/equal-height.js
+++ assets/js/equal-height.js
@@ -10,58 +10,80 @@
 
 "use strict";
 
-/**
- * Equal height for cards
- */
-document.addEventListener('DOMContentLoaded', (e) => {
+const EqualHeight = {};
 
-    const equalHeight = (parent, child) => {
-        const parentElements = document.querySelectorAll(parent);
+EqualHeight.observe = (container, childSelector) => {
+    if (!container) return;
 
-        for (const parentContainer of parentElements) {
-            const children = parentContainer.querySelectorAll(child);
-            if (children.length > 0) {
-                let currentTallest = 0;
-                let currentRowStart = 0;
-                let rowDivs = [];
-                let topPosition = 0;
+    let timer = null;
 
-                for (const childElement of children) {
-                    childElement.style.height = 'auto'; // Reset height
-                    topPosition = childElement.getBoundingClientRect().top;
-
-                    if (currentRowStart !== topPosition) {
-                        // Set all heights in the previous row
-                        for (const div of rowDivs) {
-                            div.style.height = `${currentTallest}px`;
-                        }
-
-                        // Reset for the next row
-                        rowDivs = [];
-                        currentRowStart = topPosition;
-                        currentTallest = childElement.offsetHeight;
-                        rowDivs.push(childElement);
-                    } else {
-                        rowDivs.push(childElement);
-                        currentTallest = Math.max(currentTallest, childElement.offsetHeight);
-                    }
-                }
-
-                // Set the height for the last row
-                for (const div of rowDivs) {
-                    div.style.height = `${currentTallest}px`;
-                }
-            }
+    const ro = new ResizeObserver(() => {
+        // Clear the timeout if there is one
+        if (timer !== null) {
+            clearTimeout(timer);
         }
-    };
 
-    equalHeight('.equal-height', '.card');
+        // Set the new timer
+        timer = setTimeout(() => {
+            timer = null;
+            EqualHeight.run(container, childSelector);
+        }, 100);
+    });
 
-    const allEvents = ["load", "resize", "orientationchange", "vueupdate"];
+    ro.observe(container);
 
-    for (const evt of allEvents) {
-        window.addEventListener(evt, (e) => {
-            equalHeight('.equal-height', '.card');
-        });
+    // Initial
+    EqualHeight.run(container, childSelector);
+};
+
+
+EqualHeight.run = (container, childSelector) => {
+    const children = container.querySelectorAll(childSelector);
+    if (!children.length) return;
+
+    let currentTallest = 0;
+    let currentRowStart = null;
+    let rowDivs = [];
+
+    for (const childElement of children) {
+        childElement.style.height = 'auto';
+        const topPosition = childElement.offsetTop;
+
+        const tolerance = 10;
+
+        // Check if the element is in the same row as the previous one
+        if (currentRowStart === null || Math.abs(currentRowStart - topPosition) > tolerance) {
+            // The element is the first in the row, so set the height to the current tallest element
+            for (const div of rowDivs) {
+                div.style.height = `${currentTallest}px`;
+            }
+
+            rowDivs = [];
+            currentRowStart = topPosition;
+            currentTallest = childElement.offsetHeight;
+            rowDivs.push(childElement);
+        } else {
+            // The element is not the first in the row, so add it to the rowDivs array
+            rowDivs.push(childElement);
+            currentTallest = Math.max(currentTallest, childElement.offsetHeight);
+        }
     }
+
+    for (const div of rowDivs) {
+        div.style.height = `${currentTallest}px`;
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const PARENT_SELECTOR = '.equal-height';
+    const CHILD_SELECTOR = '.card';
+
+    const containers = document.querySelectorAll(PARENT_SELECTOR);
+
+    for (const container of containers) {
+        EqualHeight.observe(container, CHILD_SELECTOR);
+    }
+
 }, false);
